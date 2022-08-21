65775715970130f6b393c1d7f4eab4f268e5ad78
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-08-21 15:58:39 +0300

    first commit

---
+++ components/header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    it's header
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'header-component'
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div>My blog navigation bar here</div>
+    <Nuxt />
+  </div>
+</template>
+
+<script>
+import vHeader from '@/components/header'
+
+export default {
+  components: {
+    vHeader
+  }
+}
+</script>
+
+<style>
+/* #app {
+
+} */
+</style>
+
