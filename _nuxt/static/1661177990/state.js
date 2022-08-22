dbb88e90da65c364914ac8598c5a1428ac837b02
SonicFear96 <SonicFear96@users.noreply.github.com>
2022-08-22 14:19:51 +0000

    deploy: 65168ecf2d6ac47c340e01131895c142d216fa83

---
+++ _nuxt/static/1661177990/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftest-nuxt-table\u002F_nuxt\u002Fstatic\u002F1661177990",layout:"default",error:a,state:{comment:[]},serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Ftest-nuxt-table\u002F",assetsPath:"\u002Ftest-nuxt-table\u002F_nuxt\u002F",cdnURL:a}}}}(null));
