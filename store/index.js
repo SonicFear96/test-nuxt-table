d6200d2d659482d641231dbd389c943a41fca11a
Bogdan Rudakov <48532613+SonicFear96@users.noreply.github.com>
2022-08-22 17:24:57 +0300

    правки (store)

--- store/index.js
+++ store/index.js
@@ -1,8 +1,13 @@
 export const state = () => ({
+  comments: [],
   comment: []
 })
 
 export const actions = {
+  async getComments({commit}, start) {
+    const comments = await this.$axios.$get(`https://jsonplaceholder.typicode.com/comments?_start=${start}&_limit=10`)
+    commit('SET_COMMENTS', comments)
+  },
   async getComment({commit}, id) {
     const comment = await this.$axios.$get(`https://jsonplaceholder.typicode.com/comments/${id}`)
     commit('SET_COMMENT', comment)
@@ -10,11 +15,15 @@ export const actions = {
 }
 
 export const mutations = {
+  SET_COMMENTS (state, value) {
+    state.comments = value
+  },
   SET_COMMENT (state, value) {
     state.comment = value
   }
 }
 
 export const getters = {
+  comments: state => state.comments,
   comment: state => state.comment
 }
